f0abb56e24df4dea822630e2dccf9593e7c00d8c
Vadim Inshakov <vadiminshakov@gmail.com>
2024-10-14 00:31:18 +0500

    refactor rotation

--- rotate.go
+++ rotate.go
@@ -1,34 +1,38 @@
 package gowal
 
 import (
+	"github.com/pkg/errors"
 	"github.com/vadiminshakov/gowal/msg"
-	"log"
-	"os"
 )
 
-// rotateSegments rotates log segment if threshold is reached.
-// If number of items in index file exceeds threshold, start writing to tmp index buffer.
-func (c *Wal) rotateSegments(newMsg msg.Msg) {
-	multiplier := 1
-	if c.segmentsNumber > 1 {
-		multiplier = c.segmentsNumber - 1
+// rotateIfNeeded rotates the log if needed.
+//
+// It opens a new segment if the number of records in the log exceeds the threshold
+// and closes oldest segment if the number of segments exceeds the limit.
+func (c *Wal) rotateIfNeeded(index uint64, key string, value []byte) error {
+	if len(c.index) < c.segmentsThreshold {
+		return nil
 	}
-	// if threshold is reached, start writing to tmp index buffer
-	if len(c.index) > c.segmentsThreshold*multiplier {
-		// if segments number exceeds the limit, flush tmp index to main index and rm oldest segment
-		if c.segmentsNumber > c.maxSegments {
-			c.index = c.tmpIndex
-			c.tmpIndex = make(map[uint64]msg.Msg)
 
-			go func(segmentName string) {
-				if err := os.Remove(segmentName); err != nil {
-					log.Printf("failed to remove oldest segment %s, error: %s", segmentName, err)
-				}
-			}(c.oldestSegName)
-			return
+	if len(c.index) >= c.segmentsNumber*c.segmentsThreshold {
+		// remove oldest segment if the number of segments exceeds the limit
+		if c.segmentsNumber >= c.maxSegments {
+			if err := c.removeOldestSegment(); err != nil {
+				return err
+			}
 		}
-		c.tmpIndex[newMsg.Index()] = newMsg
+
+		// close current segment and open new one
+		if err := c.log.Close(); err != nil {
+			return errors.Wrap(err, "failed to close log file")
+		}
+
+		if err := c.openNewSegment(); err != nil {
+			return err
+		}
 	}
 
-	return
+	c.tmpIndex[index] = msg.Msg{Key: key, Value: value, Idx: index}
+
+	return nil
 }
