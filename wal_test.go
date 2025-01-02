fdfaa6dfc6ff79f0e08c94b53c1ad70b963b00f8
Vadim Inshakov <vadiminshakov@gmail.com>
2025-01-02 16:49:59 +0500

    make code cleaner

--- wal_test.go
+++ wal_test.go
@@ -279,10 +279,10 @@ func TestChecksum_UnsafeRecover(t *testing.T) {
 	require.Error(t, compareChecksums(log.log, log.checksum))
 
 	// recover
-	erasedFiles, err := UnsafeRecover("./testlogdata", "log_")
+	removedFiles, err := UnsafeRecover("./testlogdata", "log_")
 	require.NoError(t, err)
-	require.Equal(t, 2, len(erasedFiles))
-	require.ElementsMatch(t, []string{"testlogdata/log_4", "testlogdata/log_4.checksum"}, erasedFiles)
+	require.Equal(t, 2, len(removedFiles))
+	require.ElementsMatch(t, []string{"testlogdata/log_4", "testlogdata/log_4.checksum"}, removedFiles)
 
 	require.NoError(t, os.RemoveAll("./testlogdata"))
 }
